bc735b0054a8818fe35e86e9296a09de6ecf71de
BestMordaEver <matvey@fedyay.net>
2020-09-08 19:20:30 +0300

    channel data continuity is a bitch

--- events.lua
+++ events.lua
@@ -304,10 +304,11 @@ events = {
 	voiceChannelLeave = function (member, channel) -- now remove the unwanted corpses!
 		if channel and channels[channel.id] then
 			if #channel.connectedMembers == 0 then
-				lobbies[channels[channel.id].parent].mutex:lock()
+				local mutex = lobbies[channels[channel.id].parent].mutex
+				mutex:lock()
 				channel:delete()
 				logger:log(4, "GUILD %s: Deleted %s", channel.guild.id, channel.id)
-				lobbies[channels[channel.id].parent].mutex:unlock()
+				mutex:unlock()
 			end
 		end
 	end,
