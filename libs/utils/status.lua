3be9a06497fbe8d03115eddf4acc580d8b1594b2
BestMordaEver <matvey@fedyay.net>
2020-10-02 10:07:01 +0300

    fluff

--- libs/utils/status.lua
+++ libs/utils/status.lua
@@ -1,5 +1,6 @@
 local channels = require "storage/channels"
 local lobbies = require "storage/lobbies"
+local client = require "discordia".storage.client
 
 --[[
 status generating function
@@ -13,7 +14,7 @@ return function ()
 	}
 
 	-- determine the cycle
-	local step = math.fmod(os.date("*t").min, 3)
+	local step = math.fmod(os.date("*t").min, 4)
 	
 	if step == 0 then -- people
 		local people = channels:people()
@@ -45,6 +46,16 @@ return function ()
 			lobbies .. (
 				lobbies == 1 and " lobby" or " lobbies") .. (
 				tostring(lobbies):match("69") and " (nice!)" or ""))
+		
+		self.type = 3
+	elseif step == 2 then -- lobbies
+		local guilds = #client.guilds
+		
+		self.name =
+			guilds == 0 and "the world go by" or (
+			guilds .. (
+				guilds == 1 and " guild" or " guilds") .. (
+				tostring(guilds):match("69") and " (nice!)" or ""))
 		
 		self.type = 3
 	end
