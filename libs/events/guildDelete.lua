31ba9cbc5ccf13e0f5648acb9d28db3f208919d6
BestMordaEver <matvey@fedyay.net>
2020-10-13 11:25:49 +0300

    guild cleanup fix

--- libs/events/guildDelete.lua
+++ libs/events/guildDelete.lua
@@ -6,7 +6,7 @@ local lobbies = require "storage/lobbies"
 local guilds = require "storage/guilds"
 
 return function (guild) -- same but opposite
-	guilds:remove(guild.id)
+	guilds[guild.id]:delete()
 	for _,channel in pairs(guild.voiceChannels) do 
 		if channels[channel.id] then channels[channel.id]:delete() end 
 		if lobbies[channel.id] then lobbies[channel.id]:delete() end
