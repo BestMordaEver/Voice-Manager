e3480397fd3b281ae67c7308dd50763801c188bf
BestMordaEver <matvey@fedyay.net>
2020-11-19 19:14:40 +0200

    fixed logging error with empty guild names

--- libs/events/guildCreate.lua
+++ libs/events/guildCreate.lua
@@ -3,5 +3,5 @@ local guilds = require "storage/guilds"
 
 return function (guild) -- triggers whenever new guild appears in bot's scope
 	guilds:add(guild.id)
-	client:getChannel("676432067566895111"):send(guild.name.." added me!\n")
+	client:getChannel("676432067566895111"):send((guild.name or "no name").." added me!\n")
 end
