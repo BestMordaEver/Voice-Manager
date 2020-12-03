4a2ff9d73bd7b4869c0e34422cc2a6e6d0c86e2d
BestMordaEver <matvey@fedyay.net>
2020-12-03 14:17:34 +0200

    added basic code for intermediate commands

---
+++ libs/commands/intermediate/matchmaking.lua
@@ -0,0 +1,22 @@
+local locale = require "locale"
+local client = require "discordia".storage.client
+
+return function (message)
+	local guild = message.content:match("matchmaking%s*(.-)$")
+	
+	if guild == "" then
+		guild = message.guild
+	else
+		guild = client:getGuild(guild)
+	end
+	
+	if not guild then
+		message:reply(locale.badServer)
+		return "Didn't find the guild"
+	end
+	
+	if guild and not guild:getMember(message.author) then
+		message:reply(locale.notMember)
+		return "Not a member"
+	end
+end
