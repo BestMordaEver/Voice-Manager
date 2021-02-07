79470f26bc0d834228e7ac6e601c0d595161ad9c
BestMordaEver <matvey@fedyay.net>
2021-02-08 01:49:37 +0200

    fixed companion name

--- libs/commands/companions/name.lua
+++ libs/commands/companions/name.lua
@@ -2,7 +2,7 @@ local locale = require "locale"
 local lobbies = require "storage/lobbies"
 
 return function (message, channel, name)
-	name = name:gsub(string.lower)
+	name = name:lower()
 	name = name:gsub("%s+", "%-")
 	lobbies[channel.id]:setCompanionTemplate(name)
 	return "Companion name template set", "ok", locale.nameConfirm:format(name)
