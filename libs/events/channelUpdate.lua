6e9321ea7b9bba733c6aa7a697588286367c6bd6
BestMordaEver <matvey@fedyay.net>
2021-03-06 16:50:32 +0200

    removed unused requires

--- libs/events/channelUpdate.lua
+++ libs/events/channelUpdate.lua
@@ -1,12 +1,5 @@
-local client = require "client"
-local logger = require "logger"
-local guilds = require "storage/guilds"
-local lobbies = require "storage/lobbies"
 local channels = require "storage/channels"
-local bitfield = require "utils/bitfield"
 local enforceReservations = require "funcs/enforceReservations"
-
-local permission = require "discordia".enums.permission
 
 return function (member, channel)
 	if channel and channels[channel.id] then
