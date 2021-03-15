9673f7ece78ac5ce5c756616f7df5de32a97c0a2
BestMordaEver <matvey@fedyay.net>
2021-03-15 14:50:42 +0200

    bitrate adapts to server boosts

--- libs/commands/lobbies/bitrate.lua
+++ libs/commands/lobbies/bitrate.lua
@@ -1,10 +1,18 @@
 local locale = require "locale"
 local lobbies = require "storage/lobbies"
 
+local tierRate = {96,128,256,384}
+local tierLocale = {"bitrateOOB","bitrateOOB1","bitrateOOB2","bitrateOOB3"}
+
 return function (message, channel, bitrate)
 	bitrate = tonumber(bitrate)
-	if not bitrate or bitrate < 8 or bitrate > 96 then
-		return "bitrate OOB", "warning", locale.bitrateOOB
+	local tier = message.guild.premiumTier
+	for _,feature in ipairs(message.guild.features) do
+		if feature == "VIP_REGIONS" then tier = 4 end
+	end
+	
+	if not bitrate or bitrate < 8 or bitrate > tierRate[tier] then
+		return "bitrate OOB", "warning", locale[tierLocale[tier]]
 	else
 		lobbies[channel.id]:setBitrate(bitrate*1000)
 		return "Lobby bitrate set", "ok", locale.bitrateConfirm:format(bitrate)
