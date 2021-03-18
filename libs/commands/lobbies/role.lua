1cf6316c93d242d7f12f5eb0249abd2cbfaf82fe
BestMordaEver <matvey@fedyay.net>
2021-03-18 22:31:27 +0200

    role reset fix

--- libs/commands/lobbies/role.lua
+++ libs/commands/lobbies/role.lua
@@ -2,7 +2,7 @@ local locale = require "locale"
 local lobbies = require "storage/lobbies"
 
 return function (message, lobby, role)
-	role = message.guild:getRole(role) or message.guild:getRole(role:match("%d+")) or message.guild.defaultRole
+	role = message.guild:getRole(role) or message.guild:getRole((role or ""):match("%d+")) or message.guild.defaultRole
 	if not role then
 		return "Invalid role provided", "warning", locale.roleBadInput
 	end
--- libs/commands/server/role.lua
+++ libs/commands/server/role.lua
@@ -2,7 +2,7 @@ local locale = require "locale"
 local guilds = require "storage/guilds"
 
 return function (message, role)
-	role = message.guild:getRole(role) or message.guild:getRole(role:match("%d+")) or message.guild.defaultRole
+	role = message.guild:getRole(role) or message.guild:getRole((role or ""):match("%d+")) or message.guild.defaultRole
 	if not role then
 		return "Invalid role provided", "warning", locale.roleBadInput
 	end
