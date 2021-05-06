526e0e2c1d06ce420edf1608e5f0033a8c61523e
BestMordaEver <matvey@fedyay.net>
2021-05-07 00:50:23 +0300

    adjusting text

--- libs/locale.lua
+++ libs/locale.lua
@@ -313,7 +313,7 @@ You can enable companion channels with `companion`]],
 	logConfirm = "Chat logs will be sent to %s",
 	logReset = "Disabled the chatlogs",
 	loggerLink = "`%s` room of `%s` lobby\n%s",
-	loggerWarning = "\n*This text chat will be logged*",
+	loggerWarning = "\n\n*This text chat will be logged*",
 	pastebinError = "`%s` room of `%s` lobby\nThere was an issue with pastebin, notify devs if this persists",
 	
 	-- room
