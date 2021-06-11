25442feadc41010177d2fe8d63d904257a087d1c
BestMordaEver <matvey@fedyay.net>
2021-06-11 03:36:30 +0300

    meh

--- libs/commands/companions/log.lua
+++ libs/commands/companions/log.lua
@@ -5,21 +5,21 @@ local permissionCheck = require "funcs/permissionCheck"
 local channelType = require "discordia".enums.channelType
 
 return function (message, channel, input)
-	if not input then
+	if input then
+		local logChannel = lookForChannel(message, input)
+		if not logChannel or logChannel.type ~= channelType.text then
+			return "Couldn't find the channel", "warning", locale.badChannel
+		end
+		
+		local isPermitted, logMsg, msg = permissionCheck(message, logChannel)
+		if isPermitted then
+			lobbies[channel.id]:setCompanionLog(logChannel.id)
+			return "Companion log channel set", "ok", locale.logConfirm:format(logChannel.name)
+		else
+			return logMsg, "warning", msg
+		end
+	else
 		lobbies[channel.id]:setCompanionLog()
 		return "Companion log channel reset", "ok", locale.logReset
-	end
-	
-	local logChannel = lookForChannel(message, input)
-	if not logChannel or logChannel.type ~= channelType.text then
-		return "Couldn't find the channel", "warning", locale.badChannel
-	end
-	
-	local isPermitted, logMsg, msg = permissionCheck(message, logChannel)
-	if isPermitted then
-		lobbies[channel.id]:setCompanionLog(logChannel.id)
-		return "Companion log channel set", "ok", locale.logConfirm:format(logChannel.name)
-	else
-		return logMsg, "warning", msg
 	end
 end
