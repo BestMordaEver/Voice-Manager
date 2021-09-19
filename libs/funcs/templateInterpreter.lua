54b1d0e9d3e1913e526c82efa4331102cdda6579
BestMordaEver <matvey@fedyay.net>
2021-09-19 22:59:14 +0300

    fix for default %game%

--- libs/funcs/templateInterpreter.lua
+++ libs/funcs/templateInterpreter.lua
@@ -2,8 +2,8 @@ return function (template, member, position, replacement)
 	local uname = member.user.name
 	local nickname = member.nickname or uname
 	local game = (member.activity and (member.activity.type == 0 or member.activity.type == 1)) and member.activity.name or 
-		(template:match("%%game%((.-)%)%%" or "no game"))
-	
+		(template:match("%%game%((.-)%)%%") or "no game")
+		
 	template = template:gsub("%%game%(.-%)%%", "%%game%%")
 	
 	local rt = {
