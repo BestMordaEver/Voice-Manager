bcf61b97f8b8579fcd3680ca3d182a8587746872
BestMordaEver <matvey@fedyay.net>
2021-11-08 16:25:31 +0200

    error fix for removing non-existing lobbies

--- libs/commands/matchmaking/remove.lua
+++ libs/commands/matchmaking/remove.lua
@@ -1,7 +1,12 @@
 local locale = require "locale"
+local dialogue = require "utils/dialogue"
 local lobbies = require "storage/lobbies"
 
 return function (message, channel)
-	lobbies[channel.id]:delete()
+	dialogue[message.author.id] = nil
+	if lobbies[channel.id] then
+		lobbies[channel.id]:delete()
+	end
+	
 	return "Matchmaking lobby removed", "ok", locale.matchmakingRemoveConfirm:format(channel.name)
 end
