e40181111caafacd388cc32169ffb3734a8ac1c5
BestMordaEver <matvey@fedyay.net>
2022-03-09 23:25:43 +0200

    temporary heartbeat solution

--- libs/events/heartbeat.lua
+++ libs/events/heartbeat.lua
@@ -1,6 +1,18 @@
+local config = require "config"
+local client = require "client"
+
+local channels = require "storage".channels
+
 local mercy = require "utils/mercy"
-local status = require "funcs/status"
+
+local message
 
 return function ()
-    
+    if config.heartbeat then
+		-- hearbeat happens
+        if not message then message = client:getChannel(config.heartbeatChannel):getMessage(config.heartbeatMessage) end
+		message:setContent(os.date())
+		-- hearbeat is partial? stop it!
+		if mercy:tick() or (config.dailyreboot and channels:people() == 0 and os.clock() > 86000) then mercy:kill() end
+	end
 end
