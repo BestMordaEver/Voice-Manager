0849c2d83ebf01000003889969858151a923cf8d
BestMordaEver <matvey@fedyay.net>
2022-03-12 14:24:40 +0200

    better command logging

--- libs/events/commandInteraction.lua
+++ libs/events/commandInteraction.lua
@@ -4,11 +4,28 @@ local commands = require "commands/init"
 
 local errorEmbed = require "embeds/error"
 
+local insert, concat = table.insert, table.concat
+
+local function stringer (strings, options)
+	for name, option in pairs(options) do
+		insert(strings, name)
+		if option.value then
+			insert(strings, tostring(option.value))
+		end
+		if option.options then
+			stringer(strings, option.options)
+		end
+	end
+	return strings
+end
+
 return function (interaction)
     if not interaction.guild then return end
 
     interaction:deferReply(true)
-	logger:log(4, "GUILD %s USER %s: %s command invoked", interaction.guild.id, interaction.user.id, interaction.commandName)
+
+	local commandString = concat(stringer({interaction.commandName}, interaction.options), " ")
+	logger:log(4, "GUILD %s USER %s invoked a command: %s", interaction.guild.id, interaction.user.id, commandString)
 
 	-- call the command, log it, and all in protected call
 	local res, logMsg, reply = xpcall(commands, debug.traceback, interaction)
