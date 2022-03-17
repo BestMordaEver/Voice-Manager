b4d466ce9554d77534b9abc9e6a8cc9a7ffcd762
BestMordaEver <matvey@fedyay.net>
2022-03-17 20:11:03 +0200

    help optimization

--- libs/embeds/help.lua
+++ libs/embeds/help.lua
@@ -7,73 +7,75 @@ local componentType, buttonStyle = enums.componentType, enums.buttonStyle
 local blurple = embeds.colors.blurple
 local insert = table.insert
 
-return embeds("help", function (page)
-	page = tonumber(page)
+local buttons = {
+	{
+		type = componentType.row,
+		components = {
+			{
+				type = componentType.button,
+				label = "Lobbies",
+				custom_id = "help_1",
+				style = buttonStyle.primary
+			},{
+				type = componentType.button,
+				label = "Matchmaking",
+				custom_id = "help_2",
+				style = buttonStyle.primary
+			},{
+				type = componentType.button,
+				label = "Companion",
+				custom_id = "help_3",
+				style = buttonStyle.primary
+			}
+		}
+	},{
+		type = componentType.row,
+		components = {
+			{
+				type = componentType.button,
+				label = "Room",
+				custom_id = "help_4",
+				style = buttonStyle.primary
+			},{
+				type = componentType.button,
+				label = "Chat",
+				custom_id = "help_5",
+				style = buttonStyle.primary
+			},{
+				type = componentType.button,
+				label = "Server",
+				custom_id = "help_6",
+				style = buttonStyle.primary
+			},{
+				type = componentType.button,
+				label = "Other",
+				custom_id = "help_7",
+				style = buttonStyle.primary
+			}
+		}
+	}
+}
+
+local helps = {}
+for i=0,#locale.helpTitle do
 	local embed = {
-		title = locale.helpTitle[page],
+		title = locale.helpTitle[i],
 		color = blurple,
-		description = locale.helpDescription[page],
+		description = locale.helpDescription[i],
 		fields = {}
 	}
 
-	for i, name in ipairs(locale.helpFieldNames[page]) do
+	for j, name in ipairs(locale.helpFieldNames[i]) do
 		insert(embed.fields, {
 			name = name,
-			value = locale.helpFieldValues[page][i]
+			value = locale.helpFieldValues[i][j]
 		})
 	end
 
 	insert(embed.fields, {name = locale.helpLinksTitle, value = locale.helpLinks})
+	helps[i] = embed
+end
 
-	return {
-		embeds = {embed},
-		components = {
-			{
-				type = componentType.row,
-				components = {
-					{
-						type = componentType.button,
-						label = "Lobbies",
-						custom_id = "help_1",
-						style = buttonStyle.primary
-					},{
-						type = componentType.button,
-						label = "Matchmaking",
-						custom_id = "help_2",
-						style = buttonStyle.primary
-					},{
-						type = componentType.button,
-						label = "Companion",
-						custom_id = "help_3",
-						style = buttonStyle.primary
-					}
-				}
-			},{
-				type = componentType.row,
-				components = {
-					{
-						type = componentType.button,
-						label = "Room",
-						custom_id = "help_4",
-						style = buttonStyle.primary
-					},{
-						type = componentType.button,
-						label = "Chat",
-						custom_id = "help_5",
-						style = buttonStyle.primary
-					},{
-						type = componentType.button,
-						label = "Server",
-						custom_id = "help_6",
-						style = buttonStyle.primary
-					},{
-						type = componentType.button,
-						label = "Other",
-						custom_id = "help_7",
-						style = buttonStyle.primary
-					}
-				}
-			}
-		}
-	}
+return embeds("help", function (page)
+	return {embeds = {helps[tonumber(page)]}, components = buttons}
 end)
