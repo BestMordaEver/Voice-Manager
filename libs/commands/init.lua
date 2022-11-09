2cda1ea314535819304059e3fe4b59401eb78d1b
BestMordaEver <matvey@fedyay.net>
2022-11-09 09:20:22 +0200

    commands are deferred faster

--- libs/commands/init.lua
+++ libs/commands/init.lua
@@ -27,14 +27,12 @@ return setmetatable({
 },{__call = function (self, interaction)
 	if interaction.commandType == commandType.chatInput then
 		local command, subcommand, argument = interaction.commandName, interaction.option and not interaction.option.value and interaction.option.name
+		if not undeferrable[subcommand] then interaction:deferReply(true) end
 		if interaction.option and interaction.option.options then
 			for optionName, option in pairs(interaction.option.options) do
 				if optionName ~= "lobby" then argument = option end
 			end
 		end
-
-		if not undeferrable[subcommand] then interaction:deferReply(true) end
-
 		return self[command](interaction, subcommand, argument and (argument.value or argument))
 	elseif interaction.type == commandType.user then
 		if interaction.commandName == "Invite" then
