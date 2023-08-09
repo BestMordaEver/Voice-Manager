5cedfb45c96bdc867f777f26efe4eca38f342220
BestMordaEver <matvey@fedyay.net>
2023-08-09 11:37:13 +0300

    heartbeat doesn't require present mesage anymore

--- config.lua
+++ config.lua
@@ -8,8 +8,6 @@ return {
 	statsFeed = "676432067566895111",			-- channel that will receive stats every day (guild count, active users, channels and lobbies)
 	stderr = "686261668522491980",				-- channel than will post error messages; can be nil
 	heartbeat = true,							-- perform heartbeat check?
-	heartbeatChannel = "676791988518912020",	-- channel that contains heartbeat message
-	heartbeatMessage = "933983203012771920",	-- message that will be edited every minute
 	dailyreboot = false,						-- bot will reboot whenever there are no active users, once per day
 	sendStats = true							-- send stats to bot list sites (top.gg and the likes)
 }
