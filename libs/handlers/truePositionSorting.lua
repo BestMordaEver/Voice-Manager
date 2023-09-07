6fbf68af57ad0d9a4f1787578ec0944ba7f99d89
BestMordaEver <matvey@fedyay.net>
2023-09-07 09:15:11 +0300

    refactor cleanup

--- libs/handlers/truePositionSorting.lua
+++
@@ -1,7 +0,0 @@
--- returns channels in the same order they are presented in the app
--- https://imgur.com/a/hRWM73c
-return function (a, b)
-	return (not a.category and b.category) or
-		(a.category and b.category and a.category.position < b.category.position) or
-		(a.category == b.category and a.position < b.position)
-end
