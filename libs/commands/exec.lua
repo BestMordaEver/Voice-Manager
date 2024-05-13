f449ea3b7154a56615deaeecf22d777d53908c76
BestMordaEver <matvey@fedyay.net>
2024-05-13 13:01:03 +0300

    update for the exec command

--- libs/commands/exec.lua
+++ libs/commands/exec.lua
@@ -1,11 +1,8 @@
+local pp = require "pretty-print"
 local config = require "config"
 
 local okEmbed = require "embeds/ok"
 local warningEmbed = require "embeds/warning"
-
-local function code (s)
-	return string.format("```\n%s```", s)
-end
 
 local sandbox = setmetatable({
 	client = require "client",
@@ -14,8 +11,40 @@ local sandbox = setmetatable({
 	channels = require "handlers/storageHandler".channels
 },{ __index = _G})
 
+local function code (s)
+	return string.format("```\n%s```", s)
+end
+
+local function printLine(...)
+    local ret = {}
+    for i = 1, select('#', ...) do
+        local arg = tostring(select(i, ...))
+        table.insert(ret, arg)
+    end
+    return table.concat(ret, '\t')
+end
+
+local function prettyLine(...)
+    local ret = {}
+    for i = 1, select('#', ...) do
+        local arg = pp.strip(pp.dump(select(i, ...)))
+        table.insert(ret, arg)
+    end
+    return table.concat(ret, '\t')
+end
+
 return function (interaction)
     if not config.owners[interaction.user.id] then return "Not owner", warningEmbed("You're not my father") end
+
+    local lines = {}
+
+    sandbox.print = function(...) -- intercept printed lines with this
+        table.insert(lines, printLine(...))
+    end
+
+    sandbox.p = function(...) -- intercept pretty-printed lines with this
+        table.insert(lines, prettyLine(...))
+    end
 
     local fn, syntaxError = load(interaction.option.value, "Bot", "t", sandbox)
     if not fn then return "Syntax error", warningEmbed(code(syntaxError)) end
@@ -23,5 +52,6 @@ return function (interaction)
     local success, runtimeError = pcall(fn)
     if not success then return "Runtime error", warningEmbed(code(runtimeError)) end
 
-    return "Code executed", okEmbed("Code executed")
+    lines = table.concat(lines, '\n') -- bring all the lines together
+    return "Code executed", okEmbed(code(lines))
 end
