4e313c85356993bb96f1c220a2b3ba767b065708
BestMordaEver <matvey@fedyay.net>
2024-08-22 20:09:35 +0300

    trash slash initializers

--- libs/slash/ping.lua
+++
@@ -1,6 +0,0 @@
-local pingEmbed = require "embeds/ping"
-local Date = require "discordia".Date
-
-return function (interaction)
-    return "Pong", pingEmbed(Date() - Date(Date.parseSnowflake(interaction.id)), interaction.guild or interaction.user.mutualGuilds:find(function () return true end))
-end
