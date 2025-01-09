780c8952190c948e46a0b75833ef5c2670d5536a
BestMordaEver <matvey@fedyay.net>
2025-01-09 14:17:40 +0200

    fixed channel sort method

--- libs/utils/truePositionSort.lua
+++ libs/utils/truePositionSort.lua
@@ -3,9 +3,19 @@ local channelType = require "discordia".enums.channelType
 -- returns channels in the same order they are presented in the app
 -- https://imgur.com/a/hRWM73c
 return function (a, b)
-	return (not a.category and b.category) or
-		(a.category and b.category and a.category.position < b.category.position) or
-		(a.category == b.category and
-			((a.type ~= channelType.voice and a.type ~= channelType.stageVoice and (b.type == channelType.voice or b.type == channelType.stageVoice)) or
-			a.position < b.position or a.createdAt < b.createdAt))
+	if a.category and not b.category then
+		return true
+	elseif not a.category and b.category then
+		return false
+	elseif a.category ~= b.category then
+		return a.category.position < b.category.position
+	elseif a.type ~= channelType.voice and a.type ~= channelType.stageVoice and (b.type == channelType.voice or b.type == channelType.stageVoice) then
+		return true
+	elseif (a.type == channelType.voice or a.type == channelType.stageVoice) and b.type ~= channelType.voice and b.type ~= channelType.stageVoice then
+		return false
+	elseif a.position ~= b.position then
+		return a.position < b.position
+	else
+		return a.createdAt < b.createdAt
+	end
 end
