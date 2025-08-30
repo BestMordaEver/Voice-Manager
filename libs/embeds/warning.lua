4dc7c3649095098e215e2ab6433945751cdab8a3
BestMordaEver <matvey@fedyay.net>
2025-08-30 06:55:52 +0300

    annotations

--- libs/embeds/warning.lua
+++ libs/embeds/warning.lua
@@ -4,10 +4,12 @@ local embed = require "embeds/embed"
 local yellow = embed.colors.yellow
 
 ---@overload fun(localeCarrier : table, line : textLine, ...? : string) : table
-return embed("warning", function (localeCarrier, msg, ...)
+local warning = embed("warning", function (localeCarrier, msg, ...)
 	return {embeds = {{
 		title = locale(localeCarrier.locale, "embedWarning"),
 		color = yellow,
 		description = locale(localeCarrier.locale, msg, ...)
 	}}, ephemeral = true}
 end)
+
+return warning
