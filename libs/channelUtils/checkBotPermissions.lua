6d1613af89d234490bae22fbb7b7d12c1c629518
BestMordaEver <matvey@fedyay.net>
2025-09-05 01:49:53 +0300

    old code cleanup

--- libs/channelUtils/checkBotPermissions.lua
+++ libs/channelUtils/checkBotPermissions.lua
@@ -34,36 +34,3 @@ return function (channel)
 
 	return true
 end
-
---[[return function (channel)
-	local bot = channel.guild.me
-	local channelPermissions = bot:getPermissions(channel)
-
-	if channelPermissions:has(permission.administrator) then
-		return true, okEmbed(channel.guild, "botPermissionsAdmin")
-	end
-
-	local mandatoryOk, mandatoryPermissions = checkPermissions(bot, channel,
-		permission.manageChannels,
-		permission.readMessages,
-		permission.connect,
-		permission.moveMembers)
-
-	if not mandatoryOk then
-		return false, warningEmbed(channel.guild, "botPermissionsMandatory", table.concat(mandatoryPermissions, " "), mandatoryPermissions)
-	end
-
-	local optionalOk, optionalPermissions = checkPermissions(bot, channel,
-		permission.manageRoles,
-		permission.createInstantInvite,
-		permission.sendMessages,
-		permission.embedLinks,
-		permission.attachFiles,
-		permission.speak)
-
-	if not optionalOk then
-		return true, warningEmbed(channel.guild, "botPermissionsOptional", table.concat(optionalPermissions, " "), optionalPermissions)
-	end
-
-	return true, okEmbed(channel.guild, "botPermissionsOk")
-end]]
