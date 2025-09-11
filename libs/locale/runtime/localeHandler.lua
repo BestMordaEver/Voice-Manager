c13d231b0d9e0ff78ae0954577ae52e87a1ff44c
BestMordaEver <matvey@fedyay.net>
2025-09-12 00:45:26 +0300

    fixed annotations

--- libs/locale/runtime/localeHandler.lua
+++ libs/locale/runtime/localeHandler.lua
@@ -1,14 +1,10 @@
-local handler = {
+---@overload fun(locale : localeName, line : textLine, ... : string) : string| string[]
+local handler = setmetatable({
 	["en-US"] = require "locale/runtime/en-US",
-}
-
----@param locale table
----@param line textLine
----@param ... string
----@return string | table
-return function (locale, line, ...)
-	locale = handler[locale]
-	if not locale or not locale[line] then locale = handler["en-US"] end
+}, {
+	__call = function (self, locale, line, ...)
+	locale = self[locale]
+	if not locale or not locale[line] then locale = self["en-US"] end
 	assert(locale[line], "there is no line "..line)
 	if ... then
 		assert(type(locale[line]) == "string", "this line does not support editable parameters")
@@ -16,4 +12,6 @@ return function (locale, line, ...)
 	else
 		return locale[line]
 	end
-end
+end})
+
+return handler
