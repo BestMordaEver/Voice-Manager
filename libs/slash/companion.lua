ccbde84ffcb4679852827d1aed100d87aa32f998
BestMordaEver <matvey@fedyay.net>
2025-11-05 13:04:32 +0200

    indent fix

--- libs/slash/companion.lua
+++ libs/slash/companion.lua
@@ -7,97 +7,97 @@ local lobbySelect = require "slash/lobbySelect"
 local locale = require "locale/localeHandler"
 
 return {
-		name = locale.companion,
-		description = locale.companionDesc,
-		contexts = {contextType.guild},
-		options = {
-			{
-				name = locale.view,
-				description = locale.companionViewDesc,
-				type = commandOptionType.subcommand,
-				options = {
-					{
-						name = locale.lobby,
-						description = locale.lobbyViewLobbyDesc,
-						type = commandOptionType.channel,
-						channel_types = {
-							channelType.voice
-						}
-					}
-				}
-			},
-			{
-				name = locale.enable,
-				description = locale.companionEnableDesc,
-				type = commandOptionType.subcommand,
-				options = {lobbySelect}
-			},
-			{
-				name = locale.disable,
-				description = locale.companionDisableDesc,
-				type = commandOptionType.subcommand,
-				options = {lobbySelect}
-			},
-			{
-				name = locale.category,
-				description = locale.companionCategoryDesc,
-				type = commandOptionType.subcommand,
-				options = {
-					lobbySelect,
-					{
-						name = locale.category,
-						description = locale.companionCategoryCategoryDesc,
-						type = commandOptionType.channel,
-						required = true,
-						channel_types = {
-							channelType.category
-						}
-					}
-				}
-			},
-			{
-				name = locale.name,
-				description = locale.companionNameDesc,
-				type = commandOptionType.subcommand,
-				options = {
-					lobbySelect,
-					{
-						name = locale.name,
-						description = locale.companionNameNameDesc,
-						type = commandOptionType.string,
-						required = true
-					}
-				}
-			},
-			{
-				name = locale.companionGreeting,
-				description = locale.companionGreetingDesc,
-				type = commandOptionType.subcommand,
-				options = {
-					lobbySelect,
-					{
-						name = locale.companionGreeting,
-						description = locale.companionGreetingGreetingDesc,
-						type = commandOptionType.string
-					}
-				}
-			},
-			{
-				name = locale.companionLog,
-				description = locale.companionLogDesc,
-				type = commandOptionType.subcommand,
-				options = {
-					lobbySelect,
-					{
-						name = locale.channel,
-						description = locale.companionLogChannelDesc,
-						type = commandOptionType.channel,
-						required = true,
-						channel_types = {
-							channelType.text
-						}
+	name = locale.companion,
+	description = locale.companionDesc,
+	contexts = {contextType.guild},
+	options = {
+		{
+			name = locale.view,
+			description = locale.companionViewDesc,
+			type = commandOptionType.subcommand,
+			options = {
+				{
+					name = locale.lobby,
+					description = locale.lobbyViewLobbyDesc,
+					type = commandOptionType.channel,
+					channel_types = {
+						channelType.voice
 					}
 				}
 			}
-		}
+		},
+		{
+			name = locale.enable,
+			description = locale.companionEnableDesc,
+			type = commandOptionType.subcommand,
+			options = {lobbySelect}
+		},
+		{
+			name = locale.disable,
+			description = locale.companionDisableDesc,
+			type = commandOptionType.subcommand,
+			options = {lobbySelect}
+		},
+		{
+			name = locale.category,
+			description = locale.companionCategoryDesc,
+			type = commandOptionType.subcommand,
+			options = {
+				lobbySelect,
+				{
+					name = locale.category,
+					description = locale.companionCategoryCategoryDesc,
+					type = commandOptionType.channel,
+					required = true,
+					channel_types = {
+						channelType.category
+					}
+				}
+			}
+		},
+		{
+			name = locale.name,
+			description = locale.companionNameDesc,
+			type = commandOptionType.subcommand,
+			options = {
+				lobbySelect,
+				{
+					name = locale.name,
+					description = locale.companionNameNameDesc,
+					type = commandOptionType.string,
+					required = true
+				}
+			}
+		},
+		{
+			name = locale.companionGreeting,
+			description = locale.companionGreetingDesc,
+			type = commandOptionType.subcommand,
+			options = {
+				lobbySelect,
+				{
+					name = locale.companionGreeting,
+					description = locale.companionGreetingGreetingDesc,
+					type = commandOptionType.string
+				}
+			}
+		},
+		{
+			name = locale.companionLog,
+			description = locale.companionLogDesc,
+			type = commandOptionType.subcommand,
+			options = {
+				lobbySelect,
+				{
+					name = locale.channel,
+					description = locale.companionLogChannelDesc,
+					type = commandOptionType.channel,
+					required = true,
+					channel_types = {
+						channelType.text
+					}
+				}
+			}
+		},
 	}
+}
